e2612f729332b70bfb8bda17430a3c61776e89c3
김진평 <jp@gimjinpyeong-ui-MacBookPro.local>
2023-09-20 14:38:42 +0900

    prettier 설정에 맞게 tsx 파일 수정

--- src/components/pages/Sites/Sites.tsx
+++ src/components/pages/Sites/Sites.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Sites = () => {
   return <div>Sites</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import ReactDOM from "react-dom/client";
-import Router from "./Router";
-import GlobalStyle from "./globalStyle/GlobalStyle";
+import ReactDOM from 'react-dom/client';
+import Router from './Router';
+import GlobalStyle from './globalStyle/GlobalStyle';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <>
     <GlobalStyle />
     <Router />
-  </>
+  </>,
 );

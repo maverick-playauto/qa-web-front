bc88db3fca0359b0633d749a845a7429749e91fe
김진평 <jp@gimjinpyeong-ui-MacBookPro.local>
2023-12-20 00:45:18 +0900

    atomic 패턴 걷어내기

--- src/components/organisms/TopBar.tsx
+++
@@ -1,8 +0,0 @@
-//import { NormalButton } from '../atoms/Button/Button.style';
-import SearchForm from '../molecules/SearchForm';
-
-const TopBar = () => {
-  return <section>null</section>;
-};
-
-export default TopBar;
--- src/pages/MainPage/Mainpage.tsx
+++ src/pages/MainPage/Mainpage.tsx
@@ -1,16 +1,5 @@
-import { NormalButton } from 'components/atoms/Button/Button';
-
-import { NormalSelect } from 'components/atoms/Select/Select';
-import { NormalText } from 'components/atoms/Text/Text';
-
 const MainPage = () => {
-  return (
-    <>
-      <NormalButton variant="primary" />
-      <NormalSelect variant="primary" width="large" />
-      <NormalText variant="primary" name="searchShop" />
-    </>
-  );
+  return null;
 };
 
 export default MainPage;
